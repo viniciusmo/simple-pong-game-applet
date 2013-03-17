11408c42210a20b56475728b0698ee47b151a24e
viniciusmo <vinicus.moises.oliveira@gmail.com>
2013-03-17 12:40:36 -0300

    Maneira de adicionar os listeners de forma mais facil

--- simple-pong-game/src/com/viniciusmo/pong/PongGame.java
+++ simple-pong-game/src/com/viniciusmo/pong/PongGame.java
@@ -3,12 +3,17 @@ package com.viniciusmo.pong;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.viniciusmo.pong.audio.PlayerMP3;
 import com.viniciusmo.pong.game.GameArea;
 import com.viniciusmo.pong.game.GameOverException;
 import com.viniciusmo.pong.game.Moveable;
 import com.viniciusmo.pong.listeners.BarControllerMovement;
+import com.viniciusmo.pong.listeners.GameControllerKeys;
+import com.viniciusmo.pong.listeners.GameKeyListener;
 import com.viniciusmo.pong.shapes.Circle;
 import com.viniciusmo.pong.shapes.Rectangle;
 import com.viniciusmo.pong.shapes.Shape;
@@ -19,34 +24,51 @@ public class PongGame extends Applet implements Runnable {
 	private Shape bar;
 	private Shape ball;
 	private GameArea area;
+	private GameKeyListener gameKeyListeners;
 	private boolean gamePlay;
 	private int score;
 
 	public void init() {
 		area = new GameArea(800, 600);
-		bar = new Rectangle(100, 0, 10, 150);
-		ball = new Circle(area, 500, 100, 15, 15);
 		setSize(area.getWidth(), area.getHeight());
 		setBackground(Color.black);
-		addKeyListener(new BarControllerMovement(bar));
-		new Thread(this).start();
-		startGame();
+		addKeyListeners();
+	}
+
+	private void addKeyListeners() {
+		List<KeyListener> keyListeners = new ArrayList<KeyListener>();
+		keyListeners.add(new GameControllerKeys(this));
+		gameKeyListeners = new GameKeyListener(keyListeners);
+		addKeyListener(gameKeyListeners);
 	}
 
 	public void startGame() {
+		bar = new Rectangle(100, 0, 10, 150);
+		ball = new Circle(area, 500, 100, 15, 15);
 		gamePlay = true;
+		gameKeyListeners.addKeyListener(new BarControllerMovement(bar));
+		new Thread(this).start();
 	}
 
 	public void stopGame() {
 		gamePlay = false;
 	}
 
+	public boolean isPlayingGame() {
+		return gamePlay;
+	}
+
 	public void paint(Graphics g) {
-		bar.draw(g);
-		ball.draw(g);
-		g.setColor(Color.WHITE);
-		g.drawString("Score: " + score, 20, 20);
-		g.drawString("by viniciusmo", getWidth() - 100, 20);
+		if (gamePlay) {
+			g.setColor(Color.WHITE);
+			bar.draw(g);
+			ball.draw(g);
+			g.drawString("Score: " + score, 20, 20);
+			g.drawString("by viniciusmo", getWidth() - 100, 20);
+		} else {
+			g.setColor(Color.WHITE);
+			g.drawString("Pressione a tecla espaço para iniciar o jogo", 20, 20);
+		}
 	}
 
 	private void sleep() {
